0dceabf7678fba1160fed29af0065c941f49e2f4
Pizza421 <77181176+Pizza421@users.noreply.github.com>
2021-01-21 02:11:59 +0000

    Create AlllAboutMe.c

---
+++ AlllAboutMe.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hi, my name is Christopher Koo.");
+    printf(" My favorite color is green.");
+    printf(" I am 9 years old.");
+    printf(" I have 2 siblings.");
+    printf(" Their names are Elizabeth and Katie.");
+    printf(" My favorite food is hamburgers.");
+    printf(" I like to play soccer.");
+    printf(" I also like to play video games.");
+    printf(" My favorite dessert is ice cream");
+
+
+    return (0);
+}
