1071822083bcbd9b434bbe46d702b3673f85b15b
mehek500 <77173485+mehek500@users.noreply.github.com>
2021-01-22 23:21:59 -0800

    Mehek Chauhan commit

---
+++ allAboutMe.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+  char name[32] = "Mehek";
+
+  char videoGame[32] = "Fortnite";
+
+  int age;
+  age = 10;
+
+  int grade;
+  grade = 5;
+
+  char FavoriteSubject[32] = "Math";
+
+  printf("Hi! My name is %s. I am %d years old. My favorite video game is %s. It is a really good video game. In school, I am in %dth grade. I find that grade a bit easy. My favorite subject in school is %s because it is very easy.", name, age, videoGame, grade, FavoriteSubject);
+  return 0;
+}
