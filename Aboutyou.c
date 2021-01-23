6620e0d4b612e212f4c1ea3e1cb67b37b9998f68
parin1127 <77180662+parin1127@users.noreply.github.com>
2021-01-23 09:13:21 -0800

    Create Aboutyou.c

---
+++ Aboutyou.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hi, my name is Parin.");
+    printf(" My favorite color is purple/blue .");
+    printf(" I am 15 years old.");
+    printf(" I have 1 siblings.");
+    printf(" His name is Param.");
+    printf(" My favorite food is ramen.");
+    printf(" I like to play soccer.");
+    printf(" I also like to play video games.");
+    printf(" My favorite dessert is ice cream");
+
+
+    return (0);
+}
