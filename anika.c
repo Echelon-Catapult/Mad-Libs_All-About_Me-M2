4fbbf42e99e7774f35c563f9dc6c8215e4d95001
anikakanigeri <77178792+anikakanigeri@users.noreply.github.com>
2021-01-25 16:56:10 -0800

    Create anika.c

---
+++ anika.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Hi there! My name is Anika.");
+    printf(" My favorite color is yellow.");
+    printf(" I am 12 years old, and I am in 7th grade.");
+    printf(" I have 1 sibling,a younger brother...no pets.");
+    printf(" My favorite food is any kind of noodles and garlic bread.");
+    printf(" My favorite sweet is a white macadamia pizookie!!");
+    printf(" I love to read fantasy/action books.");
+    printf(" I love coding, writing, and learning new things.");
+    printf(" My favorite thing to do is spend time with my family!");
+    printf(" That's all about me!!");
+ 
+    return (0);
+}
